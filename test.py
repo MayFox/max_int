31b46e54cfb4e934d560844c972dfc30ec1c0c08
MayFox <thorgerdur18@ru.is>
2019-09-05 09:29:13 +0000

    Algorithm written for max_int

---
+++ test.py
@@ -0,0 +1,12 @@
+n = int(input(" "))
+
+star = str("*")
+counter = str("*")
+
+while n >= 0:
+    for i in range(1, 100):
+        i = counter
+        i + counter 
+        print(star + counter)
+
+        
